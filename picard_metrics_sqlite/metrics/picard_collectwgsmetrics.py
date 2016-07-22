b0c2c744d1c5b8466c078f215a0cd556d9528c49
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-07-21 19:27:02 -0500

    init

--- picard_metrics_sqlite/metrics/picard_collectwgsmetrics.py
+++ picard_metrics_sqlite/metrics/picard_collectwgsmetrics.py
@@ -2,7 +2,7 @@ import os
 
 from .metrics_util import picard_select_tsv_to_df
 
-def picard_CollectWgsMetrics_metrics_to_df(stats_path, logger):
+def picard_CollectWgsMetrics_to_df(stats_path, logger):
     select = 'GENOME_TERRITORY'
     df = picard_select_tsv_to_df(stats_path, select, logger)
     return df
@@ -15,7 +15,7 @@ def picard_CollectWgsMetrics_histogram_to_df(stats_path, logger):
 
 def run(uuid, stats_path, bam, fasta, input_state, engine, logger, metric_name):
     table_name = 'picard_' + metric_name
-    df = picard_CollectWgsMetrics_metrics_to_df(stats_path, logger)
+    df = picard_CollectWgsMetrics_to_df(stats_path, logger)
     if df is None:
         sys.exit('No metrics collected from: %s' % stats_path)
     else:
