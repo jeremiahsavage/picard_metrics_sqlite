57452d87e6d1efdb0ffcd2e5168c78409a65b8dc
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-09-03 18:09:25 -0500

    cleanup

--- picard_metrics_sqlite/metrics/picard_collecthsmetrics.py
+++ picard_metrics_sqlite/metrics/picard_collecthsmetrics.py
@@ -2,19 +2,19 @@ import os
 
 from .metrics_util import picard_select_tsv_to_df
 
-def picard_CollectHsMetrics_to_df(stats_path, logger):
+def picard_CollectHsMetrics_to_df(metric_path, logger):
     select = 'BAIT_SET'
-    df = picard_select_tsv_to_df(stats_path, select, logger)
+    df = picard_select_tsv_to_df(metric_path, select, logger)
     return df
 
-def picard_CollectHsMetrics_histogram_to_df(stats_path, logger):
+def picard_CollectHsMetrics_histogram_to_df(metric_path, logger):
     select = 'coverage'
-    df = picard_select_tsv_to_df(stats_path, select, logger)
+    df = picard_select_tsv_to_df(metric_path, select, logger)
     return df
 
-def run(uuid, stats_path, bam, bam_library, bait, target, fasta, input_state, engine, logger, metric_name):
+def run(uuid, metric_path, bam, bam_library, bait, target, fasta, input_state, engine, logger, metric_name):
     table_name = 'picard_' + metric_name
-    df = picard_CollectHsMetrics_to_df(stats_path, logger)
+    df = picard_CollectHsMetrics_to_df(metric_path, logger)
     df['bait'] = bait
     df['bam'] = bam
     df['fasta'] = fasta
@@ -25,7 +25,7 @@ def run(uuid, stats_path, bam, bam_library, bait, target, fasta, input_state, en
     df.to_sql(table_name, engine, if_exists='append')
 
     table_name = 'picard_' + metric_name + '_histogram'
-    df = picard_CollectHsMetrics_histogram_to_df(stats_path, logger)
+    df = picard_CollectHsMetrics_histogram_to_df(metric_path, logger)
     df['bait'] = bait
     df['bam'] = bam
     df['fasta'] = fasta
