127657337244b72ce7ad7fb1ba69ee5143a2197b
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-09-03 18:16:13 -0500

    cleanup

--- picard_metrics_sqlite/metrics/picard_collectalignmentsummarymetrics.py
+++ picard_metrics_sqlite/metrics/picard_collectalignmentsummarymetrics.py
@@ -2,21 +2,17 @@ import os
 
 from .metrics_util import picard_select_tsv_to_df
 
-def picard_CollectAlignmentSummaryMetrics_to_df(stats_path, logger):
+def picard_CollectAlignmentSummaryMetrics_to_df(metric_path, logger):
     select = 'CATEGORY'
-    df = picard_select_tsv_to_df(stats_path, select, logger)
+    df = picard_select_tsv_to_df(metric_path, select, logger)
     return df
 
-
-def run(uuid, stats_path, bam, fasta, input_state, engine, logger, metric_name):
+def run(uuid, metric_path, bam, fasta, input_state, engine, logger, metric_name):
     table_name = 'picard_' + metric_name
-    df = picard_CollectAlignmentSummaryMetrics_to_df(stats_path, logger)
-    if df is None:
-        sys.exit('No metrics collected from: %s' % stats_path)
-    else:
-        df['uuid'] = uuid
-        df['bam'] = bam
-        df['input_state'] = input_state
-        df['fasta'] = fasta
-        df.to_sql(table_name, engine, if_exists='append')
+    df = picard_CollectAlignmentSummaryMetrics_to_df(metric_path, logger)
+    df['bam'] = bam
+    df['fasta'] = fasta
+    df['input_state'] = input_state
+    df['uuid'] = uuid
+    df.to_sql(table_name, engine, if_exists='append')
     return
