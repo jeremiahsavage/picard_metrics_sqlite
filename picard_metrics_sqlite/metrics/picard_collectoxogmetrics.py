7c30deb704fcaaec813e1ed39a37eee9f466b0ed
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-09-03 18:17:05 -0500

    cleanup

--- picard_metrics_sqlite/metrics/picard_collectoxogmetrics.py
+++ picard_metrics_sqlite/metrics/picard_collectoxogmetrics.py
@@ -2,22 +2,18 @@ import os
 
 from .metrics_util import picard_select_tsv_to_df
 
-def picard_CollectOxoGMetrics_to_df(stats_path, logger):
+def picard_CollectOxoGMetrics_to_df(metric_path, logger):
     select = 'SAMPLE_ALIAS'
-    df = picard_select_tsv_to_df(stats_path, select, logger)
+    df = picard_select_tsv_to_df(metric_path, select, logger)
     return df
 
-
-def run(uuid, stats_path, bam, fasta, vcf, input_state, engine, logger, metric_name):
+def run(uuid, metric_path, bam, fasta, vcf, input_state, engine, logger, metric_name):
     table_name = 'picard_' + metric_name
-    df = picard_CollectOxoGMetrics_to_df(stats_path, logger)
-    if df is None:
-        sys.exit('No metrics collected from: %s' % stats_path)
-    else:
-        df['uuid'] = uuid
-        df['bam'] = bam
-        df['input_state'] = input_state
-        df['fasta'] = fasta
-        df['vcf'] = vcf
-        df.to_sql(table_name, engine, if_exists='append')
+    df = picard_CollectOxoGMetrics_to_df(metric_path, logger)
+    df['bam'] = bam
+    df['fasta'] = fasta
+    df['input_state'] = input_state
+    df['uuid'] = uuid
+    df['vcf'] = vcf
+    df.to_sql(table_name, engine, if_exists='append')
     return
