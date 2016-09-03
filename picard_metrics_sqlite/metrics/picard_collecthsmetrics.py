e64f6efb43251eabdfb5fb0c30cb4602a141db26
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-09-03 18:05:09 -0500

    alpha reorder

--- picard_metrics_sqlite/metrics/picard_collecthsmetrics.py
+++ picard_metrics_sqlite/metrics/picard_collecthsmetrics.py
@@ -15,23 +15,23 @@ def picard_CollectHsMetrics_histogram_to_df(stats_path, logger):
 def run(uuid, stats_path, bam, bam_library, bait, target, fasta, input_state, engine, logger, metric_name):
     table_name = 'picard_' + metric_name
     df = picard_CollectHsMetrics_to_df(stats_path, logger)
-    df['uuid'] = uuid
-    df['bam'] = bam
-    df['library'] = bam_library
     df['bait'] = bait
-    df['target'] = target
-    df['input_state'] = input_state
+    df['bam'] = bam
     df['fasta'] = fasta
+    df['input_state'] = input_state
+    df['library'] = bam_library
+    df['target'] = target
+    df['uuid'] = uuid
     df.to_sql(table_name, engine, if_exists='append')
 
     table_name = 'picard_' + metric_name + '_histogram'
     df = picard_CollectHsMetrics_histogram_to_df(stats_path, logger)
-    df['uuid'] = uuid
-    df['bam'] = bam
-    df['library'] = bam_library
     df['bait'] = bait
-    df['target'] = target
-    df['input_state'] = input_state
+    df['bam'] = bam
     df['fasta'] = fasta
+    df['input_state'] = input_state
+    df['library'] = bam_library
+    df['target'] = target
+    df['uuid'] = uuid
     df.to_sql(table_name, engine, if_exists='append')    
     return
