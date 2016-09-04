22b55fcf86421712e9fb92005939b70b70a21ab1
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-09-03 22:07:34 -0500

    store exome_kit, not bait & target

--- picard_metrics_sqlite/metrics/picard_collecthsmetrics.py
+++ picard_metrics_sqlite/metrics/picard_collecthsmetrics.py
@@ -12,26 +12,24 @@ def picard_CollectHsMetrics_histogram_to_df(metric_path, logger):
     df = picard_select_tsv_to_df(metric_path, select, logger)
     return df
 
-def run(uuid, metric_path, bam, bam_library, bait, target, fasta, input_state, engine, logger, metric_name):
+def run(uuid, metric_path, bam, bam_library, exome_kit, fasta, input_state, engine, logger, metric_name):
     table_name = 'picard_' + metric_name
     df = picard_CollectHsMetrics_to_df(metric_path, logger)
-    df['bait'] = bait
     df['bam'] = bam
+    df['exome_kit'] = exome_kit
     df['fasta'] = fasta
     df['input_state'] = input_state
     df['library'] = bam_library
-    df['target'] = target
     df['uuid'] = uuid
     df.to_sql(table_name, engine, if_exists='append')
 
     table_name += '_histogram'
     df = picard_CollectHsMetrics_histogram_to_df(metric_path, logger)
-    df['bait'] = bait
     df['bam'] = bam
+    df['exome_kit'] = exome_kit
     df['fasta'] = fasta
     df['input_state'] = input_state
     df['library'] = bam_library
-    df['target'] = target
     df['uuid'] = uuid
     df.to_sql(table_name, engine, if_exists='append')    
     return
