5074a638530e8bcac70657483279daa4a646574c
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2017-07-19 16:47:05 -0500

    call proper function

--- picard_metrics_sqlite/metrics/picard_collectrnaseqmetrics.py
+++ picard_metrics_sqlite/metrics/picard_collectrnaseqmetrics.py
@@ -24,7 +24,7 @@ def run(run_uuid, metric_path, bam, fasta, ref_flat, ribosomal_intervals, input_
     df.to_sql(table_name, engine, if_exists='append')
 
     table_name += '_histogram'
-    df = picard_CollectHsMetrics_histogram_to_df(metric_path, logger)
+    df = picard_CollectRnaSeqMetrics_histogram_to_df(metric_path, logger)
     df['bam'] = bam
     df['fasta'] = fasta
     df['input_state'] = input_state
